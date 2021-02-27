2928bac8e48d2b3c9f04f64e0df76fbaffa46991
Mateus Ferreira <mateusanjos225@gmail.com>
2021-02-27 17:51:40 -0300

    Projeto Finalizado

--- src/main/resources/db/migration/V04.0__criar_tabela_pessoa_salaevento.sql
+++ src/main/resources/db/migration/V04.0__criar_tabela_pessoa_salaevento.sql
@@ -6,12 +6,10 @@ CREATE TABLE pessoa_salaevento(
 );
 
 
-INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (1, 2);
+
 INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (1, 3);
 INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (3, 2);
-INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (3, 3);
 INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (2, 1);
-INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (2, 2);
 INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (4, 1);
-INSERT INTO pessoa_salaevento (pessoa_id, salaevento_id) VALUES (4, 3);
+
 
